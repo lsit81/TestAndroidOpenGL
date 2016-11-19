9a7b190ae23fbff28c706fc801759874ae2243a8
seungtaek.lim <devlsit81@gmail.com>
2016-11-19 22:38:33 +0900

    opengl 1.0, 2.0 테스트 화면 추가

--- app/src/main/java/com/lsit/android/testopengl/MainActivity.java
+++ app/src/main/java/com/lsit/android/testopengl/MainActivity.java
@@ -1,7 +1,15 @@
 package com.lsit.android.testopengl;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.lsit.android.testopengl.gl.v1.OpenGLES10Activity;
+import com.lsit.android.testopengl.gl.v2.OpenGLES20Activity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button_opengl_v1)
+    protected void onClickOpenglV1() {
+        Intent intent = new Intent(this, OpenGLES20Activity.class);
+        startActivity(intent);
+    }
+
+    @OnClick(R.id.button_opengl_v2)
+    protected void onClickOpenglV2() {
+        Intent intent = new Intent(this, OpenGLES10Activity.class);
+        startActivity(intent);
     }
 }
